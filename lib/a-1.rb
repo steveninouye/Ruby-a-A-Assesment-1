2356b01632aa040d82a072d9f6e7d4b49f8268da
Steven Inouye <steveninouye@msn.com>
2018-10-03 21:04:52 -0700

    completed assignment

--- lib/a-1.rb
+++ lib/a-1.rb
@@ -37,7 +37,11 @@ class Hash
   # above. Do not use Hash#select in your method.
 
   def my_select(&prc)
-
+    hash = Hash.new
+    self.each do |key, val|
+      hash[key] = val if prc.call(key,val)
+    end
+    hash
   end
 end
 
@@ -46,13 +50,52 @@ end
 # a given string. If there is no palindrome longer than two letters, return false.
 
 def longest_palindrome(string)
+  num = 0
+  arr = string.chars
+  arr.each_with_index do |ltr, idx|
+    if ltr == string[idx + 1]
+      num = 2 if num < 2
+      i = 1
+      frst = idx - i
+      lst = idx + 1 + i
+      while string[frst] && string[lst] && palindrome?(string[frst..lst])
+        i += 1
+        num = i * 2
+        frst = idx - i
+        lst = idx + 1 + i
+      end
+    elsif ltr == string[idx+2]
+      num = 3 if num < 3
+      i = 1
+      frst = idx - i
+      lst = idx + 2 + i
+      while string[frst] && string[lst] && palindrome?(string[frst..lst])
+        i += 1
+        num = i * 2 + 1
+        frst = idx - i
+        lst = idx + 1 + i
+      end
+    end
+  end
+  return false if num == 0
+  num
+end
 
+def palindrome?(str)
+  str == str.reverse
 end
 
 class Array
   # Write a method that calls a passed block for each element of the array
   def my_each(&prc)
-
+    i = 0
+    arr = Array.new
+    while self[i]
+      arr[i] = prc.call(self[i])
+      i += 1
+    end
+    return self if !arr[i]
+    arr
   end
 end
 
@@ -60,7 +103,20 @@ class Array
   # Write an Array method that returns a bubble-sorted copy of an array.
   # Do NOT call the built-in Array#sort method in your implementation.
   def bubble_sort(&prc)
-
+    prc ||= Proc.new{|num1,num2| num1 <=> num2}
+    sorted = false
+    arr = self.dup
+    until sorted
+      sorted = true
+      arr.each_with_index do |el, idx|
+        break if idx + 1 == arr.length
+        if prc.call(arr[idx], arr[idx+1]) > 0
+          sorted = false
+          arr[idx], arr[idx+1] = arr[idx+1], arr[idx] 
+        end
+      end
+    end
+    arr
   end
 
   # You are not required to implement this; it's here as a suggestion :-)
@@ -75,14 +131,50 @@ end
 # object-oriented programming skills!
 
 class Cat
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
 
+  def meow
+    "meow"
+  end
+
+  def receive_love
+    meow
+  end
 end
 
 class Dog
+  attr_accessor :name, :breed
   BREEDS = ['Husky', 'Black Labrador', 'German Shepherd', 'Chihuahua']
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def play_with(cat)
+    cat.receive_love
+  end
+
+  def self.make_a_pup
+    Dog.new("pup", BREEDS.sample)
+  end
 
 end
 
 class Human
+  def initialize(name)
+    @name = name
+    @happy = false
+  end
 
+  def happy?
+    @happy
+  end
+
+  def receive_love
+    @happy = true
+  end
 end
